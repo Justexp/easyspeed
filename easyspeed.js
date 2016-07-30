10bd1704c2023cb201341361fafe4c033559b33b
Justexp <growlinsnk@gmx.com>
2016-07-30 08:09:17 +0200

    work in progress

--- easyspeed.js
+++ easyspeed.js
@@ -10,7 +10,15 @@ var EasySpeed = function(var positionObj, var destinationObj, unitpersec, mspers
 
     this.distance = Math.sqrt(this.distanceX*this.distanceX + this.distanceY*this.distanceY);
 
-    this.speed = (this.distance/unitpersec)*mspersec;
+    this.speed = Math.floor((this.distance/unitpersec)*mspersec);
 
     return this.speed;
 }
+
+// unitpersec / mspersec -> 10 / 1000 = 0.01
+// pos.x += 0.01   pos.y += 0.01
+// if x distance is 200 and y distance is 50
+// pos.x += 0.01 and
+// if(time % 4){
+//   pos.y += 0.01
+// } ratio compensation
